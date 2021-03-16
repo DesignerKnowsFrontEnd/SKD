42dee218fff9130ae7bc5eabd0683401f789cf94
DesignerKnowsFrontEnd <s.kennedy@magloo.com>
2021-03-16 16:40:38 +0200

    restructred sass folder and files to be more organised

--- homepage.php
+++ homepage.php
@@ -7,9 +7,49 @@
 
 $global = get_field('global_options', 'options');
 global $post;
+
+$hero = get_field('content_group');
+$heroSlider = get_field('slider_content_group');
+
 get_header(); ?>
 
-<h2>Homepage</h2>
+<section class="hero">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h6><?php echo $hero['pre_header'];?></h6>
+                <h1><?php echo $hero['title'];?></h1>
+                <p><?php echo $hero['paragraph'];?></p>
+                <a href='<?php echo esc_url($hero['button']['url']); ?>'><?php echo $hero['button']['title'];?></a> 
+            </div>
+        </div>
+    </div>
+
+    <div class="slider-main">
+        <!-- Swiper -->
+        <div id='sliderMain' class="swiper-container">
+            <div class="swiper-wrapper">
+                <?php if( have_rows('slider_content_group')) : ?>
+                <?php while( have_rows('slider_content_group')): the_row(); 
+                    $sliderImage = get_sub_field('image');  
+                    $sliderTilte = get_sub_field('title');  
+                    $sliderBtn = get_sub_field('button');  
+                ?>
+                <div class="swiper-slide overlay">
+                    <img src="<?php echo esc_url($sliderImage['url']);  ?>" alt="<?php echo esc_attr($sliderImage['alt']);  ?>" class='mobile-img'/>
+                </div>
+                <?php endwhile; ?>
+                <?php endif; ?>
+            </div>
+            <!-- Add Pagination -->
+            <div class="swiper-pagination"></div>
+            <!-- Add Arrows -->
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+        </div>
+    </div>
+
+</section>
     
 
     
---
+++ testpage.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template Name: Test Page
+ *
+ */
+
+
+
+global $post;
+get_header(); ?>
+
+
+<section class="hero">
+    <div class="hero-grid">
+        <div class="left-content">
+            <h1>Left Content</h1>
+        </div>
+        <div class="right-content">
+            <h1>Right Content</h1>
+        </div>
+    </div>
+</section>
+
+
+<?php get_footer(); ?>
