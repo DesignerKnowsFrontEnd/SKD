878cde695c8078f8305f6ee609e1509d4df0cd94
DesignerKnowsFrontEnd <s.kennedy@magloo.com>
2021-03-16 15:01:11 +0200

    added custom responsive mobile navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,9 +3,19 @@
 
 
 
+// SIDEBAR NAV Function
 
-
-
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("sidebarNav").style.width = "100%";
+    document.getElementById("sidebarNav").style.right = "0";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("sidebarNav").style.width = "0";
+    document.getElementById("sidebarNav").style.right = "-150px";
+  }
 
 
 
--- header.php
+++ header.php
@@ -13,51 +13,63 @@
 
 
 <body <?php body_class(); ?>>
-    <header>
-        <div class="top-nav">
-            <div class="container-fluid">
-                <div class="top-nav-inner">
-                    <div class="contact-info">
-                        <a href="#"><?php echo $contact['email_icon']; ?>
-                            <?php echo $contact['email_label']; ?></a>
-                        <div class='divider'></div>
-                        <a href="#"><?php echo $contact['phone_icon']; ?>
-                            <?php echo $contact['phone_label']; ?></a>
-                    </div>
-                    <div class="social-icons">
-                        <a href="<?php echo $social['facebook_link']; ?>"><?php echo $social['facebook_icon']; ?></a>
-                        <a href="<?php echo $social['instagram_link']; ?>"><?php echo $social['instagram_icon']; ?></a>
-                        <a href="<?php echo $social['twitter_link']; ?>"><?php echo $social['twitter_icon']; ?></a>
-                        <a href="<?php echo $social['tripadvisor_link']; ?>"><?php echo $social['tripadvisor_icon']; ?></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <nav id='navbar_top' class="navbar navbar-expand-lg navbar-light" role="navigation">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="/">
-                    <img src="<?php echo $global['site_logo']['url']; ?>" alt="<?php echo $global['site_logo']['alt']; ?>" class="logo">
-                </a>
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <button class="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <?php wp_nav_menu(
-                        array(
-                            'menu' => 'Main Menu'
-                            ,'container'       => 'div'
-                            ,'container_class' => 'collapse navbar-collapse'
-                            ,'container_id'    => 'bs-example-navbar-collapse-1'
-                            ,'menu_class'      => 'navbar-nav ml-auto'
-                            ,'depth' => 2
-                            ,'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback'
-                            ,'walker' => new WP_Bootstrap_Navwalker()
-                        )
-                    ); 
-                ?>
 
-            </div>
-        </nav>
-    </header>
+<header class='header'>
+    <div class="header-Logo">
+           <img src="<?php echo $global['site_logo']['url']; ?>" alt="<?php echo $global['site_logo']['alt']; ?>" class="logo">
+    </div>
+
+    <?php
+        $menuParameters = array(
+            'menu' => 'primary_menu',
+            'before'        => '',
+            'after'     => '',
+            'echo'            => false,
+            'container'       => 'div',
+            'container_class'       => 'header-Links',
+            'depth'           => 0,
+        );
+
+echo strip_tags(wp_nav_menu( $menuParameters ), '<a><div>' );
+?>
+    <div class="header-CTA">
+        <a href="#" class='btn btn-primary btn-header'>Hire Me</a>
+    </div>
+    <div class="header-MobileBtn">
+        <a href="javascript:void(0);" class="icon" onclick="openNav()">
+                <i class="bi bi-list"></i>
+        </a>
+    </div>
+</header>    
+
+<div id='sidebarNav' class="sidebarNav">
+    <div class="sidebarNav-flex">
+        <div class="sidebarNav-Links">
+            <a href="#" class="header-Link active">home</a>
+            <a href="#" class="header-Link">about</a>
+            <a href="#" class="header-Link">portfolio</a>
+            <a href="#" class="header-Link">articles</a>
+            <a href="#" class="header-Link">contact</a>
+        </div>
+        <div class="header-CloseBtn">
+            <a href="javascript:void(0);" class="closebtn" onclick="closeNav()">
+                <i class="bi bi-x"></i>
+            </a>
+        </div>
+    </div>
+    
+    <div class="sidebarNav-CTAs">
+        <div class="social-links">
+            <a href="#" class='social-link'>Fb</a>
+            <a href="#" class='social-link'>In</a>
+            <a href="#" class='social-link'>Ln</a>
+            <a href="#" class='social-link'>Be</a>
+            <a href="#" class='social-link'>Gh</a>
+        </div>
+        <div class="header-CTA">
+            <a href="#" class='btn btn-primary btn-header'>Hire Me</a>
+        </div>
+    </div>
+</div>
+
+
